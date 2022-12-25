2f53782fb8cf584ae40f6ad0b3928326e56db122
BlueBigTech <blue.bigtech@gmail.com>
2022-12-24 22:22:01 -0500

    google-auth, googlesheet-update funcions

---
+++ python-write-googlesheet.py
@@ -0,0 +1,102 @@
+"""
+Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# [START sheets_update_values]
+from __future__ import print_function
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '13ZLuggfary1hg41HjrFKquAArgGjzcehhsAL3V726hk'
+
+# creds = None
+
+def google_auth():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('cert/authorizedToken.json'):
+        creds = Credentials.from_authorized_user_file('cert/authorizedToken.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'cert/GoogleAppToken.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('cert/authorizedToken.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+def update_values(creds, spreadsheet_id, range_name, value_input_option, _values):
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+        """
+    # creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+    try:
+
+        service = build('sheets', 'v4', credentials=creds)
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        # [START_EXCLUDE silent]
+        values = _values
+        # [END_EXCLUDE]
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+if __name__ == '__main__':
+    creds = google_auth()
+    # Pass: spreadsheet_id,  range_name, value_input_option and  _values
+    update_values(creds, 
+                  SPREADSHEET_ID,
+                  "PersonalData!A2:C2", "USER_ENTERED",
+                  [
+                      ['Brent', 'Toronto', 'BigThibk']
+                  ])
+    # [END sheets_update_values]
